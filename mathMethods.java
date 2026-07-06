6768ba373a3e7da5dbf37d90408a16579d67ad67
Jacob Seadorf <bloggingpenguin@gmail.com>
2026-07-05 21:55:00 -0400

    Created mathMethods.java

---
+++ mathMethods.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MathMethods {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      double x;
+      double y;
+      double z;
+      x = scnr.nextDouble();
+      y = scnr.nextDouble();
+      z = scnr.nextDouble();
+
+      System.out.print(Math.pow(x,z) + " "); //x^z
+      System.out.print(Math.pow(x, Math.pow(y, z)) + " "); //x^y^z
+      System.out.print(Math.abs(y) + " "); // |y|
+      System.out.println(Math.sqrt(Math.pow(x * y, z))); //sqrt(xy)^z
+   }
+}
